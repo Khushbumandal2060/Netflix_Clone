872641d4fc577090af955bdc2a334ca243eed334
khushi <khusbumandal2060@gmail.com>
2025-08-22 00:42:13 +0545

    Pages and Components

---
+++ src/components/Navbar/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ' ./Titlecards.css'
+const TitleCards = () => {
+  return (
+    <div className='titlecards'>
+      
+    </div>
+  )
+}
+
+export default TitleCards
---
+++ src/pages/Home/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ' ./Login.css'
+const login = () => {
+  return (
+    <div className='login'>
+      
+    </div>
+  )
+}
+
+export default login
