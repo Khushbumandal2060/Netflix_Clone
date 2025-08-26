af11a1300b8f2e9f88e58141f7998793e880477c
khushi <khusbumandal2060@gmail.com>
2025-08-27 00:35:38 +0545

    Paragraph

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import hero_title from '../../assets/hero_title.png'  // <-- make sure this file exists
+
 const Home = () => {
   return (
     <div className='home'>
-      <Navbar/>
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="Hero Banner" className='banner-img' />
+        
+        <div className="hero-caption">
+          <img src={hero_title} alt="Hero Title" className='caption-img' />
+          <p>
+            Youth is the most energetic and vibrant stage of human life, often seen as the backbone of a nation. 
+            It is a period filled with dreams, ambitions, creativity, and the power to bring change. 
+          </p>
+          <div className="hero-btns">
+            <button className='btn'>
+              <img src={play_icon} alt="Play Icon" /> Play
+            </button>
+            <button className='btn dark-btn'>
+              <img src={info_icon} alt="Info Icon" /> More Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
